123883017a461585f7490c134f5aa377744b5df8
andrewprivate <32175943+andrewprivate@users.noreply.github.com>
2024-09-06 13:27:58 -0500

    Delete web directory

--- web/src/main.mjs
+++
@@ -1,8 +0,0 @@
-import { Controller } from "./ui/Controller.mjs";
-
-window.controller = new Controller();
-const element = window.controller.getElement();
-document.body.appendChild(element); // Append the controller's UI to the body
-
-element.style.width = '100%'; // Set the width of the controller's UI
-element.style.height = '100%'; // Set the height of the controller's UI
