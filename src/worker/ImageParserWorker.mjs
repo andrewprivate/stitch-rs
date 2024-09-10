bcfa05b5ddf23d26be40522bdcd3a92017e1085b
Andrew S <32175943+andrewprivate@users.noreply.github.com>
2024-09-10 14:09:03 -0500

    Update

--- src/worker/ImageParserWorker.mjs
+++
@@ -1,9 +0,0 @@
-import { GrayImage3D } from "../Image.mjs";
-import { WorkerMessageHandler } from "./WorkerMessageHandler.mjs";
-
-const messageHandler = new WorkerMessageHandler(self);
-
-messageHandler.on('file2Image', async (file, extension, progressCallback) => {  
-    const image = await GrayImage3D.fromBlobFile(file, extension, progressCallback);
-    return await image.transfer();
-});
